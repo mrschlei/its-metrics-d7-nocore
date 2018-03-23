0f617990c0623d3a6280f5670556364437ddb121
Ben Fairfield <benfa@umich.edu>
2018-03-23 17:53:26 -0400

    start.sh will not run drush

--- start.sh
+++ start.sh
@@ -14,12 +14,10 @@ ln -sf /secrets/ssl/USERTrustRSACertificationAuthority.pem /etc/ssl/certs/USERTr
 ln -sf /secrets/ssl/AddTrustExternalCARoot.pem /etc/ssl/certs/AddTrustExternalCARoot.pem
 ln -sf /secrets/ssl/sha384-Intermediate-cert.pem /etc/ssl/certs/sha384-Intermediate-cert.pem
 
-##schleif edit - commenting out this loop, in case there are chown/chmod things in local.start.sh
 if [ -f /secrets/app/local.start.sh ]
 then
   /bin/sh /secrets/app/local.start.sh
 fi
-##end schleif comment-out'ing
 
 ## Rehash command needs to be run before starting apache.
 c_rehash /etc/ssl/certs
@@ -35,6 +33,6 @@ a2ensite default-ssl
 
 cd /var/www/html
 #drush @sites cc all --yes
-drush up --no-backup --yes
+#drush up --no-backup --yes
 
 /usr/local/bin/apache2-foreground
